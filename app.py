6569299e37cd1af74756c46e145aa63ed98e38a6
ljdyer <74581995+ljdyer@users.noreply.github.com>
2021-10-13 05:27:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
+
 # app.py
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
+# Enable Cross-Origin Resource Sharing
 CORS(app)
 
+
+# ========================================
 @app.route('/getmsg/', methods=['GET'])
 def respond():
+    """If a non-empty string is provided, return the same string in title case"""
+
     # Get name from URL parameter and prepare response
     text = request.args.get("text", None)
     response = {}
@@ -20,6 +27,7 @@ def respond():
     return jsonify(response)
 
 
+# ========================================
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
